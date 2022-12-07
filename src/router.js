a957a676acb53f15c079eeb150c950b54c4dbd34
Kichotee <pt.timone@gmail.com>
2022-12-08 00:50:09 +0100

    deployment trial-3

--- src/router.js
+++ src/router.js
@@ -1,32 +1,32 @@
-import{createRouter, createWebHistory} from  'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import home from './views/Home.vue'
 import works from './views/Works.vue'
 import about from './views/About.vue'
 
 
+const router = createRouter({
 
-const routes =[
-    {
-        path:'/',
-        name:'home',
-        component: home
-        
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: home
 
-    },
-    {
-        path:'/Works',
-        name:'works',
-        component:works
-    },
-    {
-        path:'/About',
-        name:'about',
-        component:about
-    },
-   
-]
-const router =createRouter({
-    routes,
-  history: createWebHistory(import.meta.env.BASE_URL)
+
+        },
+        {
+            path: '/Works',
+            name: 'works',
+            component: works
+        },
+        {
+            path: '/About',
+            name: 'about',
+            component: about
+        },
+
+    ]
 })
+
 export default router
