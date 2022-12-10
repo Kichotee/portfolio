13f92d45b177f0f31ac626f49303f6dd02915ee0
Kichotee <pt.timone@gmail.com>
2022-12-10 13:57:37 +0100

    responsiveness fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -88,7 +88,7 @@ export default {
     .container-box {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
-        grid-auto-rows: 200px;
+        grid-auto-rows: 50%;
         height: 100%;
     }
 
@@ -102,8 +102,8 @@ export default {
     
 
     img {
-        width: 60%;
-        height: 100%;
+        width: 80%;
+        
 
     }
 }
